dbe0f0b3acfdf56441c1e3ac76472c7030532947
kjul <juliankunschke@yahoo.de>
2021-01-31 11:36:12 +0100

    add actual sax, remove paa

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import streamlit as st
 import pandas as pd
-from data_prep import sax
+import numpy as np
+from data_prep import sax, cuts_for_asize
 from PIL import Image
+import string
 
 st.title("SAX Showcase")
 
@@ -13,42 +15,30 @@ dataset_paths = {
     "temperatures": "data/daily-min-temperatures.csv",
     "daily female births": "data/daily-total-female-births.csv"
 }
-
 univar_ts = pd.read_csv(dataset_paths[dataset], index_col="index")
 col_name = univar_ts.columns[0]
+size_of_alphabet = st.sidebar.slider("Alphabet size:", 2, 20)
 
-possible_frames = [i for i in range(1, 27) if len(univar_ts) % i == 0]
-frame_selection = st.sidebar.slider("Granularity:", 1, len(possible_frames))
-number_frames = possible_frames[frame_selection-1]
-
-st.markdown("### Original Time Series")
+sd = np.std(univar_ts)
+mean = np.mean(univar_ts)
+cuts = np.array(cuts_for_asize(size_of_alphabet))
+sax_cuts = [i * sd + mean for i in cuts]
+sax_cuts.pop(0)
+alphabet = list(string.ascii_lowercase) 
+for i, j in enumerate(sax_cuts):
+    univar_ts["upper bound of " + alphabet.pop(0)] = int(j)
 st.dataframe(univar_ts)
-st.line_chart(univar_ts[col_name])
-st.markdown("### Piecewise Aggregate Approximation")
-st.write(f"Selected number of frames: {number_frames}")
-
-univar_ts = univar_ts.sort_values(col_name)
-sax_col, paa_col = sax(univar_ts[col_name], number_frames)
-univar_ts["sax"] = sax_col
-univar_ts["paa"] = paa_col
-univar_ts = univar_ts.sort_index()
-st.line_chart(univar_ts[[col_name, "paa"]])
+st.line_chart(univar_ts)
 
 st.markdown("### Symbolic Aggregate Approximation")
-result_sax = ""
-for i in univar_ts.sax:
-    result_sax += i
+univar_ts["sax"] = sax(univar_ts[col_name], size_of_alphabet)
+result_sax = "".join(univar_ts.sax)
 st.markdown("```" + result_sax + "```")
 
-sax_summary = pd.DataFrame(set(list(zip(sax_col, paa_col)))).sort_values(1).reset_index(drop=True)
-sax_summary.columns = ["sax", "mean_of_frame"]
-st.write("Mean of the original time series' values in each frame:")
-st.write(sax_summary)
-
-frequencies = {}
-for i in set(sax_col):
-    frequencies[i] = [sax_col.count(i)]
 st.write("Frequency of SAX symbols:")
+frequencies = {}
+for i in set(list(result_sax)):
+    frequencies[i] = [result_sax.count(i)]
 st.write(pd.DataFrame(frequencies))
     
 image = Image.open('lisa_sax.gif')
